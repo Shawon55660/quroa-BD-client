981c521f2a6cc05b679500cc0e4b6c48f80a7d86
shawonahme <shawonahmed55660@gmail.com>
2024-12-23 03:51:47 +0600

    comment added

--- src/allComponents/Card.jsx
+++ src/allComponents/Card.jsx
@@ -1,14 +1,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import axios from 'axios';
 import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Comment from './Comment';
 
 
 const Card = ({post}) => {
     const   {_id,owner_email,productName,productBrand,productImageURL,queryTitle,boycottReason,recommendationCount,currentData,ownerInfo} = post;
+
+    const[comment,setComment] = useState([])
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    const fetchData = async()=>{
+        const {data} = await axios.get(`${import.meta.env.VITE_localURL}/recommend-id?recommand_id=${_id}`)
+        setComment(data)
+    }
+    console.log(comment)
     return (
-        <div>
-            <div className="card bg-base-100 w-96 shadow-xl">
+        <div className='  '>
+            <div className="p-2 bg-base-100 card border-[1px]     w-96 shadow-xl">
                 <figure>
                     <img
                         src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
@@ -19,8 +33,12 @@ const Card = ({post}) => {
                   <p>Posted Time: {format(new Date(currentData), 'Pp')}</p>
                     <p>{productName}</p>
                     <p>RecommendationCount {recommendationCount}</p>
+                   
                     <Link to={`/queries/details/${_id}`}><button className='btn btn-success'>recommend </button></Link>
                     <div className="card-actions justify-end">
+                   <p className='border-t-2'> {
+                        comment.map(comment=> <Comment key={comment._id} comment={comment}></Comment>)
+                    }</p>
                         
                     </div>
                 </div>
