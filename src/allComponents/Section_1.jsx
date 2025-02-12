212b3c024febcab71636828a8d27d6c1788700fd
shawonahme <shawonahmed55660@gmail.com>
2025-02-12 17:58:56 +0600

    ask & question sesstion added

--- src/allComponents/Section_1.jsx
+++ src/allComponents/Section_1.jsx
@@ -15,7 +15,7 @@ const Section_1 = () => {
 
                     <h2 className="text-2xl font-bold py-3 text-[#EF4444]">Ask A Question</h2>
                     <h1 className="text-3xl md:text-5xl  leading-normal font-semibold py-2">Get Help From Professionals</h1>
-                    <p className="text-gray-400 py-2">Sed commodo odio eu condimentum eleifend. Maecenas semper nisl a mattis semper. Quisque sodales laoreet interdum. Aliquam ut erat sit amet nisl lobortis maximus lorem ipsum Etiam congue, sem id vulputate condimentum, nibh arcu lobortis lorem ipsum...</p>
+                    <p className="text-gray-400 py-2">Need expert guidance? Connect with industry professionals who can provide reliable advice, answer your questions, and help you solve problems efficiently. Whether it's career advice, technical support, or personal growth, get the insights you need from experienced professionals in various fields. Start learning and growing today!</p>
                     <Link to='/my-queries'><button className='bg-[#EF4444] rounded-sm font-semibold my-4 px-4 py-2' >Add Your Query</button></Link>
                     
 
